1d478d334c480869f3ffa5eef9c614d87cc48a6c
Sunil Sharma <sunilsharma.workit@gmail.com>
2025-04-29 19:54:49 +0530

    feat: add Sidebar and MainContainer layout to Home page

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import Sidebar from '../Components/Sidebar'
+import MainContainer from '../Components/MainContainer'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className='grid grid-flow-col grid-cols-12 h-[calc(100vh-4rem)]'>
+        <Sidebar />
+        <MainContainer />
+    </div>
   )
 }
 
