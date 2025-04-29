ac1025cbc54be0772d34942c39664f8f82bd9aea
Sunil Sharma <sunilsharma.workit@gmail.com>
2025-04-29 19:54:16 +0530

    feat: integrate Redux and add WatchPage route to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,28 @@
-import React from 'react'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import AppLayout from './Layouts/AppLayout';
-import Home from './Pages/Home';
-import AboutUs from './Components/AboutUs';
+import React from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import AppLayout from "./Layouts/AppLayout";
+import Home from "./Pages/Home";
+import AboutUs from "./Components/AboutUs";
+import { Provider } from "react-redux";
+import reduxStore from "./Redux/reduxStore";
+import WatchPage from "./Pages/WatchPage";
 
 const App = () => {
   const route = createBrowserRouter([
     {
-        path: "/",
-        element: <AppLayout />,
-        children : [
-          {path:"/", element: <Home />},
-          {path:"/aboutus", element: <AboutUs />},
-        ]
-    }
-])
-  return (<RouterProvider router={route}></RouterProvider>)
-}
+      path: "/",
+      element: <AppLayout />,
+      children: [
+        { path: "/", element: <Home /> },
+        { path: "/watch", element: <WatchPage /> },
+      ],
+    },
+  ]);
+  return (
+    <Provider store={reduxStore}>
+      <RouterProvider router={route} />
+    </Provider>
+  );
+};
 
-export default App
+export default App;
