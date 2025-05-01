fade724fa7ac27031ab32e05f4cee364bdd8667d
Sunil Sharma <sunilsharma.workit@gmail.com>
2025-05-01 15:32:39 +0530

    Refactor WatchPage layout to include LiveChat component using CSS Grid

--- src/Pages/WatchPage.jsx
+++ src/Pages/WatchPage.jsx
@@ -1,10 +1,14 @@
 import CommentsContainer from "../Components/CommentsContainer";
+import LiveChat from "../Components/LiveChat";
 import WatchVideo from "../Components/WatchVideo";
 
 const WatchPage = () => {
   return (
     <div className="">
+      <div className="grid grid-cols-12 w-[95%] mx-auto mt-4 space-x-4">
       <WatchVideo />
+      <LiveChat />
+      </div>
       <CommentsContainer />
     </div>
   );
